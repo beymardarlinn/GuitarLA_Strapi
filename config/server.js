32756ec2c1510ea7876b6705eda5b2e10b6da972
unknown <darlinnb6@gmail.com>
2022-08-29 19:24:57 -0400

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e5a86181b2c5adbb4443847a95d968e3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f8b48e15-cff7-40fa-bad1-7a72af140f54'
+};
